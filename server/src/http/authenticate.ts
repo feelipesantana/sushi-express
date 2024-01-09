1048dce9289555b920ac4e744a153bc3d893103f
Felipe Santana <felipe.jesus0801@gmail.com>
2024-01-09 14:34:33 -0300

    feat: Authorization

--- server/src/http/authenticate.ts
+++ server/src/http/authenticate.ts
@@ -6,6 +6,9 @@ import { z } from 'zod'
 import { UnauthorizedError } from './routes/errors/unauthorized-error'
 import { NotAManagerError } from './routes/errors/not-a-manager-error'
 import { db } from '../db/connection'
+import { eq } from 'drizzle-orm';
+
+import { users } from '../db/schemas'
 
 const jwtPayloadSchema = z.object({
   sub: z.string(),
@@ -41,35 +44,32 @@ export const authenticate = new Elysia()
   email: z.string().email(),
   password: z.string(),
 })
-  const {email, password} = authSchemaZod.parse(body)
-
-  console.log(email, password)
-  
+  const {email, password} = authSchemaZod.parse(body)  
 
   const getEmail = await db.query.users.findFirst({
-    with:{
-      email
-    }
+    where: eq(users.email, email) && eq(users.password, password)
   })
 
-  console.log(getEmail)
-  // if(!getEmail){
-  //   throw new UnauthorizedError()
-  // }
-  // setCookie('auth', await jwt.sign(), {
-  //     httpOnly: true,
-  //     maxAge: 7 * 86400,
-  // })
+  if(!getEmail){
+    throw new UnauthorizedError()
+  }
+
+  console.log("TESTE", getEmail)
+  setCookie('auth', await jwt.sign({email}), {
+      httpOnly: true,
+      maxAge: 7 * 86400,
+  })
 
   return `Sign in as ${cookie.auth}`
 })
 .get('/profile', async ({ jwt, set, cookie: { auth } }) => {
+
   const profile = await jwt.verify(auth)
 
   if (!profile) {
-      set.status = 401
-      return 'Unauthorized'
+    set.status = 401
+    return 'Unauthorized'
   }
 
-  return `Hello ${profile.name}`
+  return `Hello ${profile.email}`
 })
