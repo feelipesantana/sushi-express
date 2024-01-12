b0eaece7f5dc54c2afd5758b04a15736ffd28892
Felipe Santana <felipe.jesus0801@gmail.com>
2024-01-12 02:22:42 -0300

    feat: authenticate sent email

--- client/src/pages/_layouts/Auth.tsx
+++ client/src/pages/_layouts/Auth.tsx
@@ -2,25 +2,25 @@ import { Outlet } from 'react-router-dom'
 
 
 
-export function AuthLayout(){
+export function AuthLayout() {
 
-        return (
-            <div className="grid min-h-screen grid-cols-2 antialiased">
-              <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-                <div className="flex items-center gap-3 text-lg text-foreground">
-                 
-                  <span className="font-bold">Sushi Express</span>
-                </div>
-                
-                <footer className="text-sm">
-                  Welcome to sushi.express -  {new Date().getFullYear()} - Done with 
-                </footer>
-              </div>
-        
-              <div className="relative flex flex-col items-center justify-center">
-              <Outlet />
-              </div>
-            </div>
-          )
-    
+  return (
+    <div className="grid min-h-screen grid-cols-2 antialiased bg-slate-200">
+      <div className="flex h-full flex-col justify-between p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+
+          <span className="font-bold">Sushi Express</span>
+        </div>
+
+        <footer className="text-sm">
+          Welcome to sushi.express -  {new Date().getFullYear()} - Done with
+        </footer>
+      </div>
+
+      <div className="relative flex flex-col items-center justify-center border-l border-foreground/10 bg-white/90 backdrop:blur-md rounded-l-[60px]">
+        <Outlet />
+      </div>
+    </div>
+  )
+
 }
--- client/src/pages/auth/SignIn.tsx
+++ client/src/pages/auth/SignIn.tsx
@@ -1,9 +1,66 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Link, redirect, useNavigate, useNavigation } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { api } from '@/services/api'
+import { useToast } from "@/components/ui/use-toast"
+import { Toaster } from '@/components/ui/toaster'
 
-export function SignIn(){
+const singFormSchema = z.object({
+    email: z.string().min(1).email()
+})
+
+type SignInForm = z.infer<typeof singFormSchema>
+export function SignIn() {
+    const { toast } = useToast()
+
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm<SignInForm>()
+
+    const handleSubmitForm = async (data: SignInForm) => {
+        console.log(data.email)
+        const email = data.email
+        try {
+            const response = await api.post("/authenticate", {
+                email
+            })
+
+            if (response.status === 200) {
+                toast({
+                    title: "Sent Email",
+                    description: "We sent a link to authenticate by your email!",
+                })
+            }
+
+        } catch (err) {
+            console.error(err)
+        }
+    }
     return (
-        <div className='p-8'>
-            <Button></Button>
+        <div className='p-8 '>
+            <Toaster />
+
+            <Button variant={'ghost'} asChild className='absolute right-8 top-8'>
+                <Link to="/sing-up">New Local</Link>
+            </Button>
+
+            <div className='flex w-[350px] flex-col justify-center gap-10'>
+                <div className='flex flex-col gap-2 text-center'>
+                    <h1 className='text-2xl font-semibold tracking-tight'>Access Dashboard</h1>
+                    <p className='text-sm text-muted-foreground'>Take care of your sales from the partner dashboard</p>
+                </div>
+                <form className='space-y-4' onSubmit={handleSubmit(handleSubmitForm)}>
+                    <div className='space-y-2'>
+                        <label className=''>Your email:</label>
+                        <Input type="email" placeholder='' className='' {...register("email")} />
+                    </div>
+                    <Button disabled={isSubmitting} className='w-full' type='submit' >Access</Button>
+                </form>
+            </div>
         </div>
     )
 }
